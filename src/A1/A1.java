d222536387cc91e67922af96c3e888d07acb13a4
Andrey Aleksovski <aleksovski_andrey@hotmail.com>
2020-07-20 17:33:16 +0200

    Initial commit

---
+++ src/A1/A1.java
@@ -0,0 +1,21 @@
+package A1;
+
+public class A1
+{
+    public static void main(String args[])
+    {
+        double bill1 = 45.00;
+        double bill2 = 35.00;
+        System.out.println("Tips: ");
+        calculateTip(bill2);
+        calculateTip(bill1);
+
+    }
+    public static void calculateTip(double bill)
+    {
+        final double rate = 2; // Consider these are percentage
+        double tip;
+        tip = bill * rate/100;
+        System.out.println("Tip: " + tip);
+    }
+}
---
+++ src/A2/A2.java
@@ -0,0 +1,18 @@
+package A2;
+
+public class A2 {
+
+    final static int z = 0;
+
+    public static void main(String[] args) {
+        System.out.println(modernMachine(5, 2));
+    }
+
+    public static float modernMachine(float n, float m) {
+        float ix;
+        m = z;
+
+        ix = n / m;
+        return ix;
+    }
+}
